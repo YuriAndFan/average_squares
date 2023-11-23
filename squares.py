8a0339d23f48f44d674fd1d8b302285e0ddcdd95
Lancelot the Brave <l.brave@spamalot.uk>
2023-11-23 10:24:34 +0000

    add a optional weights argument in terms of command line

--- squares.py
+++ squares.py
@@ -1,12 +1,14 @@
 """Computation of weighted average of squares."""
 
 from argparse import ArgumentParser
+
+
 def average_of_squares(list_of_numbers, list_of_weights=None):
-    """ Return the weighted average of a list of values.
-    
+    """Return the weighted average of a list of values.
+
     By default, all values are equally weighted, but this can be changed
     by the list_of_weights argument.
-    
+
     Example:
     --------
     >>> average_of_squares([1, 2, 4])
@@ -19,22 +21,22 @@ def average_of_squares(list_of_numbers, list_of_weights=None):
 
     """
     if list_of_weights is not None:
-        assert len(list_of_weights) == len(list_of_numbers), \
-            "weights and numbers must have same length"
+        assert len(list_of_weights) == len(
+            list_of_numbers
+        ), "weights and numbers must have same length"
         effective_weights = list_of_weights
     else:
         effective_weights = [1] * len(list_of_numbers)
     squares = [
         weight * number * number
-        for number, weight
-        in zip(list_of_numbers, effective_weights)
+        for number, weight in zip(list_of_numbers, effective_weights)
     ]
     return sum(squares)
 
 
 def convert_numbers(list_of_strings):
     """Convert a list of strings into numbers, ignoring whitespace.
-    
+
     Example:
     --------
     >>> convert_numbers(["4", " 8 ", "15 16", " 23    42 "])
@@ -51,15 +53,16 @@ def convert_numbers(list_of_strings):
 
 
 if __name__ == "__main__":
-    numbers_strings = ["1","2","4"]
-    weight_strings = ["1","1","1"]        
+    numbers_strings = ["1", "2", "4"]
+    weight_strings = ["1", "1", "1"]
     parser = ArgumentParser(description="Calculate the average of squares of numbers")
-    parser.add_argument('number', nargs='*')
+    parser.add_argument("number", nargs="*")
+    parser.add_argument("--weights", nargs="*")
 
     arguments = parser.parse_args()
     numbers = convert_numbers(arguments.number)
-    weights = convert_numbers(weight_strings)
-    
+    weights = convert_numbers(arguments.weights)
+
     result = average_of_squares(numbers, weights)
-    
+
     print(result)
