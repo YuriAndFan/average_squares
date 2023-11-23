0d73b4387ba9683e24d39dcee935af75bc0bc6a3
Lancelot the Brave <l.brave@spamalot.uk>
2023-11-23 10:20:49 +0000

    Add a positional argument from command line

--- squares.py
+++ squares.py
@@ -1,6 +1,6 @@
 """Computation of weighted average of squares."""
 
-
+from argparse import ArgumentParser
 def average_of_squares(list_of_numbers, list_of_weights=None):
     """ Return the weighted average of a list of values.
     
@@ -53,8 +53,11 @@ def convert_numbers(list_of_strings):
 if __name__ == "__main__":
     numbers_strings = ["1","2","4"]
     weight_strings = ["1","1","1"]        
-    
-    numbers = convert_numbers(numbers_strings)
+    parser = ArgumentParser(description="Calculate the average of squares of numbers")
+    parser.add_argument('number', nargs='*')
+
+    arguments = parser.parse_args()
+    numbers = convert_numbers(arguments.number)
     weights = convert_numbers(weight_strings)
     
     result = average_of_squares(numbers, weights)
